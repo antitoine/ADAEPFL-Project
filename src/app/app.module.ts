c2e6fc29d4e7d34e6dfd3cfd432c6517213f440c
Antoine Chabert <chabert.antoine@gmail.com>
2016-12-27 19:41:20 +0100

    #6 - Add the left menu and two pages - Change favicon and add logo of ADA - WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { CollapseModule } from 'ng2-bootstrap';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
 import { ScrapingComponent } from './scraping/scraping.component';
+
+const appRoutes: Routes = [
+  { path: 'main', component: MainComponent },
+  { path: 'scraping', component: ScrapingComponent },
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+  { path: '**', redirectTo: '/main', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
+    MainComponent,
     ScrapingComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    RouterModule.forRoot(appRoutes),
+    CollapseModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
