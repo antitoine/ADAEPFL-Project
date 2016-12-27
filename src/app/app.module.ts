e95e7b47a5f5b207cdc593601e8ae0b317af0d90
Antoine Chabert <chabert.antoine@gmail.com>
2016-12-27 16:46:29 +0100

    #6 - Add scraping section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ScrapingComponent } from './scraping/scraping.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScrapingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/scraping/scraping.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scraping',
+  templateUrl: './scraping.component.html',
+  styleUrls: ['./scraping.component.css']
+})
+export class ScrapingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
