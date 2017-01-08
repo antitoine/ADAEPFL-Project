7449f002a8ed0736b9fce44a53d52ad6988c815c
Antoine Chabert <chabert.antoine@gmail.com>
2017-01-08 23:30:53 +0100

    Fix issue #9 when the route change there is an auto scroll to top - Add alt message for the loading image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Subscription } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
 
   menuToggled: boolean = false;
 
+  routerSubscription: Subscription;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    // Make scroll to top when changing route
+    this.routerSubscription = this.router.events
+      .filter(event => event instanceof NavigationEnd)
+      .subscribe(event => window.scrollTo(0, 0));
+  }
+
+  ngOnDestroy() {
+    this.routerSubscription.unsubscribe();
+  }
 }
