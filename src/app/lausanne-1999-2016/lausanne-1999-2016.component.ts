6291686ca851c34b924413d191668498f410f827
Antoine Chabert <chabert.antoine@gmail.com>
2017-01-12 23:18:54 +0100

    #6 Add plotly without typescript type and npm use WIP

--- src/app/lausanne-1999-2016/lausanne-1999-2016.component.ts
+++ src/app/lausanne-1999-2016/lausanne-1999-2016.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var Plotly:any;
 
 @Component({
   selector: 'app-lausanne-1999-2016',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Lausanne19992016Component implements OnInit {
 
+  data = [{
+    values: [19, 26, 55],
+    labels: ['Residential', 'Non-Residential', 'Utility'],
+    type: 'pie'
+  }];
+
+  layout = {
+    height: 380,
+    width: 480
+  };
+
   constructor() { }
 
   ngOnInit() {
+    Plotly.newPlot('myDiv', this.data, this.layout);
   }
 
 }
