50fd24a5200758eb8efcba7572457ad46d3b76f7
Arbër Salihi <arber.salihi@epfl.ch>
2017-01-13 15:42:39 +0100

    #8 - Add subgraphes for gender analysis, add global distribution and change of comments accordingly

--- StatisticalStudy/Lausanne_1999_2016/lausanne_1999_2016_utils.py
+++ StatisticalStudy/Lausanne_1999_2016/lausanne_1999_2016_utils.py
@@ -9,8 +9,6 @@ import numpy as np
 # ----------------------------------------------------------------------------------------------------------
 # Functions
 
-# winners name can be found:
-
 def compute_overall_rank(data):
     '''
     This function computes the overall rank for a given running.
@@ -64,12 +62,11 @@ def compute_overall_rank(data):
     
     return pd.concat(all_years)
 
+
 def remove_outliers(data):
     '''
-    The method removes the outliers on the data, precisly for each category, the method remove all time smaller than the best 
-    runners in the category
-
-    The problem come from that certain people have resinged after the first loop.
+    The method removes outliers present in the data. More precisely, for each category, the method removes all times which are smaller than the best runner in the considered category.
+    Nota-bene: Remove of such outliers is necessary as certain people have resigned after the first loop, while their time was still considered.
     
     Parameters
         - data: DataFrame containing records for a given running
@@ -81,28 +78,29 @@ def remove_outliers(data):
         total_remove = 0
         all_cate = []
         
-        # Select year
+        # We select year
         year_selected = data[data['year'] == year]
         
         for category in data['category'].unique():
-            # Select by category
+            # We select by category
             category_selection = year_selected[year_selected['category'] == category]
             
-            # best time of the category
+            # We retrieve best time of the category
             best_time = (category_selection['time']
                                  [(category_selection['category'] == category) & (category_selection['rank'] == 1)])
             if best_time.empty:
                 continue
             
             best_time = best_time.values[0]
-            # remove all time smaller than the best time of the category
+            
+            # We remove all times smaller than the best time of the category
             without_outliers = category_selection[(category_selection['time'] >= best_time )] 
             
             total_remove = total_remove + (len(category_selection.index) - len(without_outliers.index))
             
             all_cate.append(without_outliers)
 
-        print('remove outliers for ' + str(year) + ' : ' + str(total_remove) + ' runners')
+        print('Number of outliers removed for ' + str(year) + ': ' + str(total_remove) + ' runners')
         
         if len(all_cate) == 0 : 
             continue
@@ -110,35 +108,94 @@ def remove_outliers(data):
                               
     return pd.concat(all_races)
 
-def generate_gender_distributions_over_years(df_10km, df_21km, df_42km):
+
+def filter_by_years(data, series):
     '''
-    This function generates the gender distributions for the different runnings over the years.
+    This function iterates over a set of data and creates series by filtering by years
+
+    Parameters
+        - data: DataFrame containing information about runners
+        - series: Dictionary to fill
+
+    Return
+        - series: Dictionary containing sets of data separated according to filter
+    '''
+
+    for year in range(1999, 2017):
+        for df_name, df in data.items():
+            series[df_name][str(year)] = len(df[df['year'] == year])
+    return series
+
+
+def filter_by_sex_and_years(data, series):
+    '''
+    This function iterates over a set of data and creates series by filtering by years and sex of runners
+
+    Parameters
+        - data: DataFrame containing information about runners
+        - series: Dictionary to fill
+
+    Return
+        - series: Dictionary containing sets of data separated according to filter
+    '''
+
+    for year in range(1999, 2017):
+        for sex in ['female', 'male']:
+            for df_name, df in data.items():
+                series[df_name][sex, str(year)] = len(df[(df['year'] == year) & (df['sex'] == sex)])
+    return series
+
+
+def generate_distributions(df_10km, df_21km, df_42km, filter):
+    '''
+    This function generates distributions for the different runnings according to a given filter.
 
     Parameters
         - df_10km: DataFrame containing all the runners over the years for the 10 km running
         - df_21km: DataFrame containing all the runners over the years for the semi-marathon
         - df_42km: DataFrame containing all the runners over the years for the marathon
+        - filter: Filter to apply in order to generate distributions (function)
 
     Return
-        DataFrame containing gender distributions over the years
+        - DataFrame containing gender distributions over the years
     '''
 
     data = {'10km': df_10km, '21km': df_21km, '42km': df_42km}
     series = {key: {} for key in data}
-    for year in range(1999, 2017):
-        for sex in ['female', 'male']:
-            for df_name, df in data.items():
-                series[df_name][(str(year), sex)] = len(df[(df['year'] == year) & (df['sex'] == sex)]) 
+    series = filter(data, series)
     return pd.DataFrame({'Marathon': pd.Series(series['42km']), 'Semi-marathon': pd.Series(series['21km']), '10 km': pd.Series(series['10km'])})
 
 
-def plot_gender_distributions_over_years(data, title="Gender distribution over the years for different runnings of Lausanne Marathon"):
+def plot_distribution_over_years(data, title='Distributions of runners over the years for Lausanne Marathon'):
+    '''
+    This function generates a graph representing distribution of runner over the years, given data.
+
+    Parameters
+        - data: DataFrame containing distribution of runners over the years
+        - title: Title of the graph (by default, 'Distributions of runners over the years for Lausanne Marathon')
+
+    Return
+        - axe: Matplotlib axes
+    '''
+
+    figure = plt.figure(figsize=(15, 5))
+    axe = plt.subplot(111)
+    axe = data.plot(kind="bar", rot=0, linewidth=0, ax=axe, stacked=True, grid=False)
+    axe.set_title(title)
+    axe.set_xlabel('Years')
+    axe.set_ylabel('Number of runners')
+    return axe
+
+
+def plot_gender_distributions_over_years(data, runnings=['Marathon', '10 km', 'Semi-marathon'], years=range(1999, 2017), title="Gender distribution over the years for different runnings of Lausanne Marathon"):
     '''
     This function generates a graph representing gender distributions over the years, given a data set.
     The following code was adapted from jrjc's code: http://stackoverflow.com/questions/22787209/how-to-have-clusters-of-stacked-bars-with-python-pandas
 
     Parameters
         - data: Array containing all DataFrames to consider (one by gender)
+        - runnings: Array of unnings to be considered and available in data (by default, 'Marathon', '10 km', 'Semi-marathon')
+        - years: Years to be considered and available in data (by default, 1999 to 2016)
         - title: Title of the graph (by default, 'Gender distribution over the years for different runnings of Lausanne Marathon')
 
     Return
@@ -146,29 +203,29 @@ def plot_gender_distributions_over_years(data, title="Gender distribution over t
     '''
 
     nb_df = len(data)
-    runnings = len(data[0].columns)
-    years = len(data[0].index) # Années
+    nb_runnings = len(runnings)
+    nb_years = len(years)
     figure = plt.figure(figsize=(15, 5))
     axe = plt.subplot(111)
 
-    for df in data : # for each data frame
+    for sex, df in data.items():
         axe = df.plot(kind="bar", linewidth=0, stacked=True, ax=axe, legend=False, grid=False)
 
 
     handles, labels = axe.get_legend_handles_labels()
-    for i in range(0, nb_df * runnings, runnings):
-        for j, pa in enumerate(handles[i:i+runnings]):
+    for i in range(0, nb_df * nb_runnings, nb_runnings):
+        for j, pa in enumerate(handles[i:i+nb_runnings]):
             # Hack: we add other rectangles
             for rect in pa.patches:
-                rect.set_x(rect.get_x() + 1 / float(nb_df + 1) * i / float(runnings))   
+                rect.set_x(rect.get_x() + 1 / float(nb_df + 1) * i / float(nb_runnings))   
                 rect.set_width(1 / float(nb_df + 1))
 
     # We add legends and title
-    axe.set_xticks((np.arange(0, 2 * years, 2) + 1 / float(nb_df + 1)) / 2.)
+    axe.set_xticks((np.arange(0, 2 * nb_years, 2) + 1 / float(nb_df + 1)) / 2.)
     axe.set_xticklabels(df.index, rotation = 0)
     axe.set_ylabel('Number of runners')
     axe.set_title(title)
-    axe.legend(handles[:runnings], labels[:runnings], loc='upper left')
+    axe.legend(handles[:nb_runnings], labels[:nb_runnings], loc='upper left')
     
     # For x axis, we manually add sex in labels
     labels = []
@@ -176,4 +233,18 @@ def plot_gender_distributions_over_years(data, title="Gender distribution over t
         formatted_label = "$\u2640$     $\u2642$\n" + label._text
         labels.append(formatted_label)
     axe.set_xticklabels(labels)
-    return axe
+
+    plt.show()
+    
+    i = 1
+    figure = plt.figure(figsize=(15, 5))
+    for sex, df in data.items():
+        df = df[runnings]
+        axe = plt.subplot(1, 2, i)
+        axe = df.plot(kind="bar", linewidth=0, stacked=True, ax=axe, grid=False)
+        axe.set_title('Distribution of ' + sex + ' runners over the years by runnings')
+        axe.set_xlabel('Years')
+        axe.set_ylabel('Number of runners')
+        i += 1
+    
+    plt.show()
