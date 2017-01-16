a7099c85821cea8d1ea7afa641d146ac62823c8d
Arnaud <arnaud.dupeyrat@gmail.com>
2017-01-16 14:21:43 +0100

    #12 - Processing data

--- StatisticalStudy/Runners/runners_utils.py
+++ StatisticalStudy/Runners/runners_utils.py
@@ -8,17 +8,17 @@ from dateutil import parser
 import datetime as dt
 from datetime import date
 
-
+QUART_MARATHON = '(Quart)|(1/4)' 
 SEMI_MARATHON = '(Semi)|(1/2)|(Halbmarathon)'
 MARATHON = 'Marathon'
 KILOMETER = '\d*(\.?|\,?)\d*(\s?|-?)[kK][mM]'
 
-OTHER_SPORT = 'Triathlon|Skating|Walking|Duathlon|Marche' 
+OTHER_SPORT = 'Triathlon|Skating|Walking|Duathlon|Marche|Cycling' 
 
 # https://live.escalade.ch/the-race/timetable-prizes-and-courses
 GENEVE_RACE = 'Course de l\'Escalade, Genève'
 # http://www.christmasmidnightrun.ch/cms/index.php?option=com_content&view=category&layout=blog&id=22&Itemid=107
-LAUSSANE_RACE = 'Christmas Midnight Run, Lausanne' 
+#LAUSSANE_RACE = 'Christmas Midnight Run, Lausanne' 
 # http://www.lausanne.ch/course_co
 OLYMPIC_RACE = 'Course capitale olympique'
 # http://www.sierre-zinal.com/la-course/
@@ -29,6 +29,8 @@ THYON_DIXENCE = 'Course pédestre Thyon-Dixence, Thyon'
 SION = 'Course Titzé de noël Sion'
 # http://www.morat-fribourg.ch/francais/classique.aspx
 MORAT = "Morat-Fribourg"
+# http://www.gruyere-cycling-tour.ch/fr/1-accueil
+CYCLIC_RACE = 'Pascal Richard Classique'
 
 FEMALE_CATEGORY_REGEX = '[D]'
 MALE_CATEGORY_REGEX = '[H]'
@@ -48,22 +50,34 @@ def compute_distance_from_category(runner):
     # There are the attribute useful to find the distance.
     attributes = ['categoryName', 'eventName', 'overCategoryName']
         
+    # Remove all other sports
     for attribute in attributes:
+        
         if pd.isnull(runner[attribute]):
             break
             
         elif (re.search(OTHER_SPORT, runner[attribute]) != None):
             return None
         
+        elif (re.search(CYCLIC_RACE, runner[attribute]) != None):
+            return None
+    
+    for attribute in attributes:
+        if pd.isnull(runner[attribute]):
+            break
+        
         # We check if the string contains information about known race.
+        elif (re.search(QUART_MARATHON, runner[attribute]) != None):
+            return 10
+        
         elif (re.search(SEMI_MARATHON, runner[attribute]) != None):
             return 21
         
         elif (re.search(MARATHON, runner[attribute]) != None):
             return 42
         
-        elif (re.search(LAUSSANE_RACE, runner[attribute]) != None):
-            return 2.4
+        #elif (re.search(LAUSSANE_RACE, runner[attribute]) != None):
+        #    return 2.4
     
         elif (re.search(GENEVE_RACE, runner[attribute]) != None):
             return 7.3
@@ -75,13 +89,13 @@ def compute_distance_from_category(runner):
             return 31
         
         elif (re.search(THYON_DIXENCE, runner[attribute]) != None):
-            return 31
+            return 16
         
         elif (re.search(SION, runner[attribute]) != None):
-            return 31
+            return 7
         
         elif (re.search(MORAT, runner[attribute]) != None):
-            return 31
+            return 17
         
         # We check if the categorie contain km expression
         match = re.search(KILOMETER, runner[attribute])
@@ -139,7 +153,6 @@ def select_runners_by_numbers_of_runs(data, nb_min_runs):
     
     # return the data with only runners with nb_min_runs minimal numbers of runs.
     return data[data['acode'].isin(group_by_acode.index.values)]
-
 
 def transform_string_to_second(runner):
     '''
@@ -234,3 +247,34 @@ def preprocess_runners(df):
         - df: DataFrame containing data
     '''
     df['gender'] = np.nan
+    
+    
+def presentation_performance_runners(fig, data, annotation = []):
+    '''
+    This function plots the speeds of a specific runner by distance.
+
+    Parameters
+        - fig: Figure on which subplots are displayed
+        - data: DataFrame containing the data relative to a given running
+        - annotation: Annotation to add in the graph (by default, no annotation)
+    '''
+    
+    plot1 = fig.add_subplot(311)
+    sns.swarmplot(x="distance (km)", y="speed (m/s)", data=data[0], ax=plot1)
+    plot1.set_title('Runner 1')
+    plot1.set_xlabel('')
+    plot1.set_ylabel('')
+
+    plot2 = fig.add_subplot(312)
+    sns.swarmplot(x="distance (km)", y="speed (m/s)", data=data[1], ax=plot2)
+    plot2.set_title('Runner 2')
+    plot2.set_xlabel('')
+    
+    # Add annotation if any given
+    if len(annotation) != 0 :
+        plot2.annotate(annotation[0], annotation[1], annotation[2], arrowprops=dict(facecolor='red', shrink=0.05))
+
+    plot3 = fig.add_subplot(313)
+    sns.swarmplot(x="distance (km)", y="speed (m/s)", data=data[2], ax=plot3)
+    plot3.set_title('Runner 3')
+    plot3.set_ylabel('')
